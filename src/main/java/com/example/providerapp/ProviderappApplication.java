16ee82241ec00dd4ab8fe7d5a6f5268890d95629
blagovesttsakovpartner <Blagovest.Tsakov@partner.bmw.de>
2026-07-11 17:13:25 +0300

    Init provider app.

---
+++ src/main/java/com/example/providerapp/ProviderappApplication.java
@@ -0,0 +1,13 @@
+package com.example.providerapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProviderappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProviderappApplication.class, args);
+	}
+
+}
